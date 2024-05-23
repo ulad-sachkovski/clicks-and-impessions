de5adbe189b4d45d52f8607705fe65d202c23703
v.sachkovskiy <e-v.sachkovskiy@homecredit.kz>
2024-05-23 17:09:40 +0200

    add spring boot files

---
+++ src/main/java/com/vsachkovsky/clickscalculation/SpringBootAppApplication.java
@@ -0,0 +1,15 @@
+package com.vsachkovsky.clickscalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class SpringBootAppApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootAppApplication.class, args);
+    }
+}
---
+++ src/test/java/com/vsachkovsky/clickscalculation/SpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsachkovsky.clickscalculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
